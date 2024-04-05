1581ea5138a477701b448868dc4f7a2c7171d09c
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2024-04-05 08:18:01 -0700

    Bump Version to 1

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Unofficial WordPress blocks to integrate Acalog with WordPress.
  * Author: BC Integration (Taija)
  * Author URI: https://www.bellevuecollege.edu
- * Version: 0.0.0 - BETA 1 #{versionStamp}#
+ * Version: 1 #{versionStamp}#
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
